78d45569963b95f899fc854d80c42e690a8e437d
Harshali Kadam <harshalikadam58@gmail.com>
2026-01-15 22:54:48 -0500

    Add Josephus problem formula solution to circular game

--- Recursions/1823_Find_The_Winner_Circular_Game.py
+++ Recursions/1823_Find_The_Winner_Circular_Game.py
@@ -13,3 +13,16 @@ class Solution(object):
             del arr[index_to_remove]
             return helper(arr,index_to_remove)
         return helper(arr,0)
+    
+    #second approach using josephus problem formula
+    class Solution(object):
+    def findTheWinner(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        survivor = 0
+        for i in range(2, n+1):
+            survivor = (survivor + k)% i
+        return survivor +1
